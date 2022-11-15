727ebc02901dfafaa1577f78004fad4ec63844d7
Loris Fiard <lfiard@gaming.tech>
2022-11-15 15:58:07 +0100

    ça marche presque

--- PFC.py
+++ PFC.py
@@ -1,17 +1,15 @@
 import random as r
 
-MenuState=False
 ScoreJoueur=0
 ScoreIA=0
-Quite=False
+Quit=False
 QuitDetect=0
 
-def Joueur(choixJoueur):
+def Joueur():
     print("Pierre = 1, Feuille = 2, Ciseau = 3")
-    choixJoueur = int(input("test"))
+    choixJoueur = int(input())
     return choixJoueur
-
-def IA (choixIA) :
+def IA() :
     choixIA=r.randint(1,3)
     if choixIA==1:
         print("L'IA a choisi Pierre")
@@ -22,64 +20,50 @@ def IA (choixIA) :
     return choixIA
 
 
-
-# On crée une fonction Result telle que
+def Result():
+    global ScoreIA
+    global ScoreJoueur
+    choixJoueur=Joueur()
+    choixIA=IA()
     
-#     Si la variable choixJoueur est égale variable choixIA alors 
-#         on affiche "Egalité !"
-
-#     Ou si la variable choixJoueur est égale à 1 et que la variable choixIA est égale à 3 alors 
-#         on affiche "Le joueur a gagné"
-#         on ajoute 1 à ScoreJoueur
-
-#     Ou si la variable choixJoueur est égale à 2 et que la variable choixIA est égale à 1 alors 
-#         on affiche "Le joueur a gagné"
-#         on ajoute 1 à ScoreJoueur
-
-#     Ou si la variable choixJoueur est égale à 3 et que la variable choixIA est égale à 2 alors 
-#         on affiche "Le joueur a gagné"
-#         on ajoute 1 à ScoreJoueur
-
-#     Ou si la variable choixJoueur est égale à 1 et que la variable choixIA est égale à 2 alors 
-#         on affiche "Le joueur a perdu"
-#         on ajoute 1 à ScoreIA
-
-#     Ou si la variable choixJoueur est égale à 2 et que la variable choixIA est égale à 3 alors 
-#         on affiche "Le joueur a perdu"
-#         on ajoute 1 à ScoreIA
-
-#     Ou si la variable choixJoueur est égale à 3 et que la variable choixIA est égale à 1 alors 
-#         on affiche "Le joueur a perdu"
-#         on ajoute 1 à ScoreIA
-
-# On crée une fonction Restart telle que
-#     On affiche "Voulez-vous rejouer ? 1 = oui, 2 = non"
-#     On définit la variable QuitDetect entrée par l'utilisateur avec la fonction Input
+    if choixJoueur==choixIA:
+        print("Egalité !")
     
-#     Si QuitDetect est égale à 1 alors
-#         On attribue la valeur Vraie à Quit
-#     Sinon
-#         On attribue la valeur Fausse à Quit
+    elif choixJoueur==1 and choixIA==3:
+        print("Le joueur a gagné")
+        ScoreJoueur+=1
 
+    elif choixJoueur==2 and choixIA==1:
+        print("Le joueur a gagné")
+        ScoreJoueur+=1
 
-# Tant que Quit est Fausse alors
+    elif choixJoueur==3 and choixIA==2:
+        print("Le joueur a gagné")
+        ScoreJoueur+=1
 
-#     Tant que MenuState est Fausse alors
+    elif choixJoueur==1 and choixIA==2:
+        print("L'IA a gagné")
+        ScoreIA+=1
 
-#         On affiche "Appuyer sur Entrée pour commencer"
-        
-#         Si la touche "Entrée" est detectée par la fonction Key alors
-#             Définir MenuState comme Vraie
-        
-#         On retourne la variable Menustate
+    elif choixJoueur==2 and choixIA==3:
+        print("L'IA a gagné")
+        ScoreIA+=1
 
+    elif choixJoueur==3 and choixIA==1:
+        print("L'IA a gagné")
+        ScoreIA+=1
 
-#     Tant que ScoreJoueur est strictement inférieur à 3 ou que ScoreIA est strictement inférieur à 3 alors
-#         On execute la fonction Joueur
-#         On execute la fonction IA
-#         On execute la fonction Result
+def Restart():
+    print("Voulez-vous rejouer ? 1 = oui, 2 = non")
+    QuitDetect = int(input())
+    if QuitDetect==1:
+        Quit==True
+    else:
+        Quit==False
     
-#     On appelle la fonction Restart
-
-
-# FIN
+while Quit==False:
+    while ScoreJoueur<3 or ScoreIA<3:
+        Joueur()
+        IA()
+        Result()
+        Restart()
